03d350f9f16e028e431b6c82f37d78ae3f15e592
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2025-11-09 17:03:14 +0530

    Day7...

--- Lab_Program/lab_3/MainAnimal.java
+++ Lab_Program/lab_3/MainAnimal.java
@@ -1,4 +1,4 @@
-package inheritence;
+package Lab_Program.lab_3;
 
 
 // multilevel inheritance
@@ -7,12 +7,13 @@ class Animal{
 		System.out.println("animal make sound");
 	}
 }
-
+// sub class 1
 class cat extends Animal{
 	void makeSound() {
 		System.out.println("Cat make meows");
 	}
 }
+//sub class 2
 class Dog extends Animal{
 	void makeSound() {
 		System.out.println("dog make bark");
@@ -22,13 +23,15 @@ public class MainAnimal {
 
 	public static void main(String[] args) {
 		Animal a= new Animal();
-		
+		//call the Animal class
 		a.makeSound();
 		
 		cat c=new cat();
+		//call the cat class
 		c.makeSound();
 		
 		Dog g= new Dog();
+		//call the Dog class
 		g.makeSound();
 	}
 
--- Lab_Program/lab_3/MainHill.java
+++ Lab_Program/lab_3/MainHill.java
@@ -1,41 +1,40 @@
-package inheritence;
-
-
+package Lab_Program.lab_3;
 class HillStations {
+    // methods
  void location() {
      System.out.println("Location is in India");
  }
-
+    // methods
  void famousFor() {
      System.out.println("Famous for its beautiful hill stations");
  }
 }
-
 class Manali extends HillStations {
+    // overriding methods
  void location() {
      System.out.println("Manali is in Himachal Pradesh");
  }
-
+    // overriding methods
  void famousFor() {
      System.out.println("It is famous for Hadimba Temple and adventure sports");
  }
 }
-
 class Mussoorie extends HillStations {
+        // overriding methods
  void location() {
      System.out.println("Mussoorie is in Uttarakhand");
  }
-
+        // overriding methods
  void famousFor() {
      System.out.println("It is famous for education institutions");
  }
 }
-
 class Gulmarg extends HillStations {
+    // overriding methods
  void location() {
      System.out.println("Gulmarg is in J&K");
  }
-
+    // overriding methods
  void famousFor() {
      System.out.println("It is famous for skiing");
  }
@@ -43,20 +42,21 @@ class Gulmarg extends HillStations {
 
 public class MainHill {
  public static void main(String[] args) {
+    // creating object of parent class
      HillStations obj;
-
+    // referencing to child class objects
      obj = new Manali();
      obj.location();
      obj.famousFor();
 
      System.out.println();
-
+    // referencing to child class objects
      obj = new Mussoorie();
      obj.location();
      obj.famousFor();
 
      System.out.println();
-
+    // referencing to child class objects
      obj = new Gulmarg();
      obj.location();
      obj.famousFor();
