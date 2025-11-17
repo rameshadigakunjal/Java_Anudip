fcd9401ced6dfa81c04c4f703aca2e43601daecf
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2025-11-17 14:51:06 +0530

    My_work

---
+++ MyWork/InventoryApp.java
@@ -0,0 +1,31 @@
+package MyWork;
+class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(int id, String productName, double startingPrice) {
+        this.id = id;
+        this.name = productName;
+        this.price = startingPrice;
+
+        System.out.println("Product Created: ID=" + id + ", Name=" + name + ", Price=" + price);
+    }
+    public int getProductId() {
+        return id;
+    }
+    public String getProductName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+}
+public class InventoryApp {
+    public static void main(String[] args) {
+        Product laptop = new Product(101, "gaming laptop", 1500.00);
+        Product smartphone = new Product(102, "smartphone", 800.00);
+
+        System.out.println(laptop.getProductName() + " costs $" + laptop.getPrice());
+    }
+}
